e892ce37a851b1f3f3d7570eda6489fa7eda287d
gabrielhipolitoo <devgabrielhipolito>
2025-03-11 18:19:36 -0300

    changes

--- src/components/common/IconsSocial.tsx
+++ src/components/common/IconsSocial.tsx
@@ -11,10 +11,21 @@ interface Props {
 const IconsSocial: FC<Props> = ({ id = "iconSocial" }) => {
   return (
     <div id={id}>
-      <a href="https://github.com/gabrielhipolitoo">{<IconGithub />}</a>
-      <a href="">{<IconInsta />}</a>
-      <a href="">{<IconLinkedin />}</a>
-      <a href="">{<IconWhats />}</a>
+      <a target="_blank" href="https://github.com/gabrielhipolitoo">
+        {<IconGithub />}
+      </a>
+      <a
+        target="_blank"
+        href="https://www.instagram.com/dev.gabrielhipolito?igsh=dm8wOWE0cXM4amg="
+      >
+        {<IconInsta />}
+      </a>
+      <a target="_blank" href="">
+        {<IconLinkedin />}
+      </a>
+      <a target="_blank" href="https://api.whatsapp.com/send?phone=71992292368">
+        {<IconWhats />}
+      </a>
       <a href="https://drive.usercontent.google.com/download?id=1jSsqEpDjpV2y_g_avHYEIU-BRjfjfMO9&export=download&authuser=1&confirm=t&uuid=d70f8d23-4114-433d-8fff-f23e76f4c252&at=AIrpjvOGewPIwego_gMnsAZ4OJHh:1736802466334">
         Meu cv
       </a>
